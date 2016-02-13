0c1c68457d86ac94bdc4f147f05d1a2b4547862b
Hao Zhang <hzhan129@illinois.edu>
2016-02-13 15:51:42 -0600

    Project setting up with Flask

---
+++ manager.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, url_for
+
+manager = Flask(__name__)
+
+
+@manager.route('/', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html', org=org_info_parse)
+
+if __name__ == '__main__':
+    manager.run(debug=True)
