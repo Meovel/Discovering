55f4b4731c5a08c0af68874403bdcfc75a737acb
Kyo Kim <kyokim751@gmail.com>
2016-02-20 14:42:26 -0600

    kkim103_shauk2: modified manager.py to deal with mongo and modified index.html

--- manager.py
+++ manager.py
@@ -1,8 +1,21 @@
 from flask import Flask, render_template, url_for
+from pymongo import MongoClient
 
 manager = Flask(__name__)
 
 org_info_parse = "Random"
+
+def getUserQuizHistory(userName):
+	resultList = []
+	client = MongoClient('localhost', 27017)
+	db = client['discovering_user_db']
+	collection = db[userName]
+	results = collection.find({"type":"quiz_result"})
+	for result in results:
+		resultList.append({'name':result['name'],'accuracy':result['score']})
+	return resultList
+
+#overall user quiz accuracy
 
 @manager.route('/', methods=['GET', 'POST'])
 def login():
@@ -11,6 +24,10 @@ def login():
 @manager.route('/index.html', methods=['GET', 'POST'])
 def index():
     return render_template('index.html', org=org_info_parse)
+
+@manager.route('/testing', methods=['POST'])
+def ajaxResponse():
+    return "greetings"
 
 if __name__ == '__main__':
     manager.run(debug=True)
