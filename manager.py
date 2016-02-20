dcda34c3ec0b03a660361ddf0f303c282c870e4b
Hao Zhang <hzhan129@illinois.edu>
2016-02-20 04:50:12 -0600

    Be able to authenticate user

--- manager.py
+++ manager.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, redirect, url_for, request, flash
+from parse_rest.connection import register
+from parse_rest.user import User
+# from flask.ext.login import login_user, login_required, logout_user, current_user
 
+# Parse setting
+application_id = 'PoSB6H1T3fxmdTEPngtYGaDnaFZsQnvBicUZt5Rc'
+rest_api_key = 'q5sYZvNdnAA6S58Dx1qqzVLOgWRJYbOqCBrqSngy'
+register(application_id, rest_api_key)
+
+# Flask setting
 manager = Flask(__name__)
+manager.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 
 @manager.route('/', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html', org=org_info_parse)
+    user = ''
+    if request.method == 'POST':
+        data = request.form
+        try:
+            user = User.login(data['username'], data['password'])
+        except:
+            flash('Incorrect username or password', 'info')
+        return redirect(url_for(""))
+    return render_template('login.html')
 
 if __name__ == '__main__':
     manager.run(debug=True)
