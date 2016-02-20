800ec4e4b7bfa71d2042f0c55b0b524249d51cba
Hao Zhang <hzhan129@illinois.edu>
2016-02-20 13:30:51 -0600

    - Update secret key - Create test template

--- manager.py
+++ manager.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, request, flash
 from parse_rest.connection import register
 from parse_rest.user import User
-# from flask.ext.login import login_user, login_required, logout_user, current_user
+from flask.ext.login import login_user, login_required, logout_user, current_user
 
 # Parse setting
 application_id = 'PoSB6H1T3fxmdTEPngtYGaDnaFZsQnvBicUZt5Rc'
@@ -10,20 +10,22 @@ register(application_id, rest_api_key)
 
 # Flask setting
 manager = Flask(__name__)
-manager.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+manager.secret_key = 'discoveringfalsksecretkey2016'
+
+if __name__ == '__main__':
+    manager.run(debug=True)
 
 
 @manager.route('/', methods=['GET', 'POST'])
 def login():
-    user = ''
+    user = None
     if request.method == 'POST':
         data = request.form
         try:
             user = User.login(data['username'], data['password'])
         except:
             flash('Incorrect username or password', 'info')
+        login_user(user)
         return redirect(url_for(""))
     return render_template('login.html')
 
-if __name__ == '__main__':
-    manager.run(debug=True)
---
+++ tests/test_login.py
@@ -0,0 +1,5 @@
+from unittest import TestCase
+
+class TestLogin(TestCase):
+    def test_login(self):
+        self.assertEqual(, )
