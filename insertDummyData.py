96154db1ab9e65d50fd3dffae05309c5a31af623
Kyo Kim <kyokim751@gmail.com>
2016-02-21 16:41:19 -0600

    kkim103: linked backend to front

--- insertDummyData.py
+++ insertDummyData.py
@@ -1,4 +1,6 @@
 from pymongo import MongoClient
+import pymongo
+from bson import json_util
 
 
 
@@ -6,10 +8,14 @@ from pymongo import MongoClient
 print 'starting script'
 client = MongoClient('localhost',27017)
 db = client['discovering_user_db']
+db.drop_collection('test_user_name')
 collection = db['test_user_name']
-dummyQuiz1 = {'name':'testQuizName1','score':90,'type':'quiz_result'}
-dummyQuiz2 = {'name':'testQuizName2','score':80,'type':'quiz_result'}
-dummyQuiz3 = {'name':'testQuizName3','score':99,'type':'quiz_result'}
+dummyQuiz1 = {'name':'testQuizName1','score':90,'type':'quiz_result','time':1}
+dummyQuiz2 = {'name':'testQuizName2','score':80,'type':'quiz_result','time':2}
+dummyQuiz3 = {'name':'testQuizName3','score':99,'type':'quiz_result','time':3}
 stuff = [dummyQuiz1, dummyQuiz2, dummyQuiz3]
 collection.insert_many(stuff)
+results = collection.find({'type':'quiz_result'}).sort([('time', pymongo.DESCENDING)])
+for result in results:
+	print json_util.dumps(result,default=json_util.default)
 print 'finished script'
