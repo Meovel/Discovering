0e27ed018cb452fe594b8e31fae495e17b2f7bf9
Riyad Shauk <riyad.shauk@gmail.com>
2016-02-21 16:54:23 -0600

    Added in some comments and a check/test in case of error, to display meaninful message

--- insertDummyData.py
+++ insertDummyData.py
@@ -1,9 +1,8 @@
+# Inserts dummy data to mongodb, currently for templates/stats.html
+# author kkim110, shauk2
 from pymongo import MongoClient
 import pymongo
 from bson import json_util
-
-
-
 
 print 'starting script'
 client = MongoClient('localhost',27017)
@@ -14,7 +13,11 @@ dummyQuiz1 = {'name':'testQuizName1','score':90,'type':'quiz_result','time':1}
 dummyQuiz2 = {'name':'testQuizName2','score':80,'type':'quiz_result','time':2}
 dummyQuiz3 = {'name':'testQuizName3','score':99,'type':'quiz_result','time':3}
 stuff = [dummyQuiz1, dummyQuiz2, dummyQuiz3]
-collection.insert_many(stuff)
+# Test to ensure database connection was valid
+try:
+    collection.insert_many(stuff)
+except: # If not valid db connection, ie, then...
+    print("Database may not be properly connected. Could not insert data")
 results = collection.find({'type':'quiz_result'}).sort([('time', pymongo.DESCENDING)])
 for result in results:
 	print json_util.dumps(result,default=json_util.default)
