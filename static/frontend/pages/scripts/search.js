7cebb83f8de5a3863529273819455c7e2d0aacc0
Kyo Kim <kyokim751@gmail.com>
2016-03-03 20:17:22 -0600

    kkim103_hao: IT3_S01 sending data to the server

--- static/frontend/pages/scripts/search.js
+++ static/frontend/pages/scripts/search.js
@@ -1,13 +1,16 @@
 
-$(document).ready(
- 	function(){
- 		$.ajax({
- 			url: "filterArea",
- 			method: "GET",
- 			dataType: "Text",
- 			data: "stuff",
- 		}).done(function(data) {
- 			alert("sent data");
- 		});
- 	}
+$(document).ready(function(){
+		$( "#filter" ).change(function () {
+			var val = $( "#filter option:selected" ).attr("value");
+			alert(val);
+			$.ajax({
+				url: "/filterArea",
+				method: "POST",
+				dataType: "Text",
+				data: val
+			}).done(function(data) {
+				
+			});
+		});
+	}
 );
