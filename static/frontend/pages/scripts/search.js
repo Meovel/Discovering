7ba3088d71ccd3dbd348b5cd5311f4526a2b57b9
Hao Zhang <hzhan129@illinois.edu>
2016-03-03 21:55:26 -0600

    update quiz query limit to 300

--- static/frontend/pages/scripts/search.js
+++ static/frontend/pages/scripts/search.js
@@ -2,14 +2,13 @@
 $(document).ready(function(){
 		$( "#filter" ).change(function () {
 			var val = $( "#filter option:selected" ).attr("value");
-			alert(val);
 			$.ajax({
 				url: "/filterArea",
 				method: "POST",
 				dataType: "Text",
 				data: val
-			}).done(function(data) {
-				
+			}).done(function(data){
+				document.write(data);
 			});
 		});
 	}
