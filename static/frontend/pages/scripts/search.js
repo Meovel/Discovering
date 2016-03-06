80cfe894fe74b35313916a05d922002654b529aa
Kyo Kim <kyokim751@gmail.com>
2016-03-05 22:42:23 -0600

    kkim103: fixed the filter output display format

--- static/frontend/pages/scripts/search.js
+++ static/frontend/pages/scripts/search.js
@@ -1,11 +1,18 @@
 function quizListToHTML(jobj){
 	var qList = jobj["data"];
 	var resultHTML= '';
+	var rowTag = '<div class="row">';
+	var rowTagEnd = '</div>';
 	for(i=0;i<qList.length;i++){
-		var htmlString = '<div class="row"><div class="col-md-4"><div class="portlet light"><div class="portlet-title"><div class="caption font-purple-plum"><img class="organization_avatar" src="./../static/organizations/book.png"><span class="caption-subject bold uppercase">';
+		var htmlString = '<div class="col-md-4"><div class="portlet light"><div class="portlet-title"><div class="caption font-purple-plum"><img class="organization_avatar" src="./../static/organizations/book.png"><span class="caption-subject bold uppercase">';
 		htmlString = htmlString+qList[i]['name']+'</span></div></div><div class="portlet-body"><div id="context" data-toggle="context" data-target="#context-menu"><p>';
 		var endString = '</p></div></div><div style="height:25px; margin-top:25px"><a class="button" style="float:left">Follow</a><a class="button" style="float:right">Quiz</a></div></div><!-- END PORTLET--></div>';
 		htmlString = htmlString + qList[i]['summary'] + endString;
+		if(i%3 === 0){
+			htmlString = rowTag + htmlString;
+		}else if(i%3 === 2 || qList.length-1 === i){
+			htmlString = htmlString + rowTagEnd;
+		}
 		resultHTML = resultHTML + htmlString;
 	}
 	return resultHTML
