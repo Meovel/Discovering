6ffe8bcbc65650298b0047605c7ba581f27fa1cc
Hao Zhang <hzhan129@illinois.edu>
2016-04-23 14:46:01 -0500

    Change button color and text after clicked for relations page

--- static/frontend/pages/scripts/favourites.js
+++ static/frontend/pages/scripts/favourites.js
@@ -8,7 +8,8 @@ $(document).ready(function () {
 			dataType: "Text",
 			data: JSON.stringify(jobj)
 		}).done(function (data) {
-			alert("done");
+			document.getElementById(orgId).innerText = "Unfollowed"
+			document.getElementById(orgId).className = "pull-right btn btn-sm btn-circle haze unfollow"
 		});
 	});
 	$("#follower").on("click","button",function(){
@@ -18,7 +19,8 @@ $(document).ready(function () {
 			url: "/deleteFollower/"+usrId,
 			method: "POST",
 		}).done(function (data) {
-			alert(data);
+			document.getElementById(usrId).innerText = "Deleted"
+			document.getElementById(usrId).className = "pull-right btn btn-sm btn-circle haze delete"
 		});
 	});
 });
